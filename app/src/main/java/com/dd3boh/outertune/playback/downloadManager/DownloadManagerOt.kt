1324da947c799427fbc88cd531ea598255a64736
Paula@2202 <rafael.de.souza@hypera.com.br>
2026-08-26 16:45:24 -0300

    fix: handle nullable download response body

--- app/src/main/java/com/dd3boh/outertune/playback/downloadManager/DownloadManagerOt.kt
+++ app/src/main/java/com/dd3boh/outertune/playback/downloadManager/DownloadManagerOt.kt
@@ -47,7 +47,7 @@ class DownloadManagerOt(
                     if (!resp.isSuccessful) {
                         throw IllegalStateException("HTTP ${resp.code}")
                     }
-                    val body = resp.body
+                    val body = resp.body ?: throw IOException("Response body is null")
                     val total = body.contentLength()
                     var downloaded = 0L
 
